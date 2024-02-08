8f63fc702930c8cee45eeb6149b236ed8bd923f0
ankitkumar619619@gmail.com <ankitkumar619619@gmail.com>
2024-02-08 22:27:32 +0530

    product app initialised

---
+++ product/webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+  },
+  target: 'web',
+  devServer: {
+    port: '3000',
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx','.js', '.jsx', '.ts', '.tsx', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/, 
+        exclude: /node_modules/, 
+        use: 'babel-loader', 
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    })
+  ]
+};
+
